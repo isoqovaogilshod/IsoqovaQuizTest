16164006dcb1a342bb0dc39e160519cd89d7d3c9
Isoqova avoqosI <isoqovaogilshod@gmail.com>
2023-10-03 09:46:34 +0000

    news

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 let questions = [
   {
-    prompt: "O'zbekiston poytaxti?",
+    question: "O'zbekiston poytaxti?",
     option: ["a.Toshkent", "b.Boku", "c.London", "d.Istanbul"],
     answer: "a",
   },
   {
-    prompt: "O'zbekiston nechta davlat bilan chegaradosh ?",
+    question: "O'zbekiston nechta davlat bilan chegaradosh ?",
     option: ["a. 3ta", "b. 4ta", "c. 5ta", "d. 6ta"],
     answer: "c",
   },
   {
-    prompt: "Dunyodagi eng uzun daryo ?",
+    question: "Dunyodagi eng uzun daryo ?",
     option: ["a.Amazonka", "b.Nil", "c.Volga", "d. Gang"],
     answer: "b",
   },
   {
-    prompt: "HTML ? ",
+    question: "HTML ? ",
     option: [
       "a.HyperText Makeup Language ",
       "b.HyperText Markup List",
@@ -25,97 +25,100 @@ let questions = [
     answer: "d",
   },
   {
-    prompt: "Pi sonini qiymatini belgilang ? ",
+    question: "Pi sonini qiymatini belgilang ? ",
     option: ["a. 3.14", "b. 3.41 ", "c. 2.71", "d. 2.17"],
     answer: "a",
   },
   {
-    prompt: "JavaScript o'zgaruvchilari qaysi usulda e'lon qilinishi mumkin:? ",
+    question:
+      "JavaScript o'zgaruvchilari qaysi usulda e'lon qilinishi mumkin:? ",
     option: ["a.var", "b.let", "c.const", "d.Hamma javob to'g'ri"],
     answer: "d",
   },
   {
-    prompt: "JavaScript nechta ma'lumot turiga ega?",
+    question: "JavaScript nechta ma'lumot turiga ega?",
     option: ["a.7 ta", "b.8 ta", "c.5 ta", "d.3 ta"],
     answer: "b",
   },
   {
-    prompt: "Nechta fasl bor? ",
+    question: "Nechta fasl bor? ",
     option: ["a.4 ta", "b.5 ta", "c.3 ta", "d.2 ta"],
     answer: "a",
   },
   {
-    prompt: "Surxondaryo markazi qayer? ",
+    question: "Surxondaryo markazi qayer? ",
     option: ["a.Jarqo'rg'on ", "b.Denov", "c.Termiz", "d.Boysun"],
     answer: "c",
   },
   {
-    prompt: "Amalni bajaring 2+2*2 ? ",
+    question: "Amalni bajaring 2+2*2 ? ",
     option: ["a.6 ", "b.8", "c.4", "d.2"],
     answer: "a",
   },
 ];
-
-while (true) {
+stopApp: while (true) {
   let menu = +prompt(
-    `Menu \n 1.Savol qo'shish \n 2.Testni boshlash \n 3.Testni tahrirlash \n 4.Testni o'chirish \n 5.Chiqish`
+    "0.Chiqish\n1.Testni boshlash \n2.Testni tahrirlash \n3.Test qo'shish \n4.Test o'chirish\n5. Savollar"
   );
-
-  if (menu == 1) {
-    enterQuestion();
-  } else if (menu == 2) {
-    quizTest();
-  } else if (menu == 3) {
-    editQuetion();
-  } else if (menu == 4) {
-    deleteQuestion();
-  } else if (menu == 5) {
-    break;
+  switch (menu) {
+    case 1:
+      let newRandomisedQuestions = Array.from(
+        getRandomisedQuestions(questions)
+      );
+      quizTest(newRandomisedQuestions);
+      break;
+    case 2:
+      editTest(questions);
+      break;
+    case 3:
+      addTest(questions);
+      break;
+    case 4:
+      deleteTest(questions);
+      break;
+    case 5:
+      alert("Savollar\n" + getQuestionsList(questions));
+      break;
+    case 0:
+      break stopApp;
+    default:
+      break;
   }
 }
 
-function enterQuestion() {
+function addTest(questions) {
   let questionNums = +prompt("Nechta savol qo'shmoqchisiz ?");
-  let vairant;
+  let variant;
   let enter_1;
 
   for (let i = 0; i < questionNums; i++) {
     enter_1 = {
-      prompt: prompt("Savolni kiriting "),
+      question: prompt("Savolni kiriting "),
       option: [],
       answer: "",
     };
     for (let i = 0; i < 4; i++) {
-      vairant = prompt(`javoblarni "a.ko'rinishida yozing`);
-      enter_1.option.push(vairant);
+      variant = prompt(`javoblarni "a.ko'rinishida yozing`);
+      enter_1.option.push(variant);
     }
     enter_1.answer = prompt("To'g'ri variantni kiriting");
     questions.push(enter_1);
   }
 }
 
-function quizTest() {
-  // random questions
-  let setNew = new Set();
-  let leng = questions.length;
-  let j = 0;
-  do {
-    j = Math.floor(Math.random() * questions.length);
-    setNew.add(questions[j]);
-  } while (setNew.size != leng);
-
-  let randomQuestions = Array.from(setNew);
-
-  // begin
-  let start = new Date();
+function quizTest(questions) {
+  let startTime = new Date();
   for (let i = 0; i < 100000; i++) {
     let ds = i * i * i * i * i * i;
   }
-
   let score = 0;
   for (let i = 0; i < questions.length; i++) {
     let response = prompt(
-      randomQuestions[i].prompt + "\n" + randomQuestions[i].option.join("\n")
+      questions[i].question +
+        "\n" +
+        questions[i].option.join("\n") +
+        "\n" +
+        `testni yakunlashni istasangiz "exit", "chiqish","cancel" ,"close","quit" kabi kalit so'zlardan foydalalning`
     );
 
     if (
@@ -138,8 +141,8 @@ function quizTest() {
       alert("Iltimos kalitlardan birini tanlang");
       i--;
     } else if (
-      response == randomQuestions[i].answer ||
-      response == randomQuestions[i].answer.toUpperCase()
+      response == questions[i].answer ||
+      response == questions[i].answer.toUpperCase()
     ) {
       score++;
     }
@@ -162,48 +165,56 @@ function quizTest() {
   } else if (score === 0) {
     alert("Siz talabalikka tavsiya etilmadingiz 😐☹️😕");
   }
-  let end = new Date();
-  alert("sarflangan vaqt: " + `${Math.floor((end - start) / 1000)}` + "soniya");
+  let endTime = new Date();
+  alert(
+    "sarflangan vaqt: " +
+      `${Math.floor((endTime - startTime) / 1000)}` +
+      "soniya"
+  );
+}
+function editTest(questions) {
+  let n = prompt(
+    "Tahrirlamochi bo'lgan testni raqamini kiriting\n" +
+      getQuestionsList(questions)
+  );
+  questions[n - 1] = {
+    question: prompt("Savolni kiriting: ", questions[n - 1].question),
+    option: [
+      prompt("a) variatni kiriting: ", questions[n - 1].option[0]),
+      prompt("b) variatni kiriting: ", questions[n - 1].option[1]),
+      prompt("c) variatni kiriting: ", questions[n - 1].option[2]),
+      prompt("d) variatni kiriting: ", questions[n - 1].option[3]),
+    ],
+    answer: prompt("to'g'ri javob: ", questions[n - 1].answer),
+  };
+  alert(getQuestionsList(questions));
 }
 
-function editQuetion() {
-  for (let i = 0; i < questions.length; i++) {
-    let answer = confirm(
-      `Tahrirlanmoqchi bo'lgan testni tanlang: \n ${questions[i].prompt}`
-    );
-
-    if (answer) {
-      let editingQuestion = questions[i];
-
-      editingQuestion.prompt = prompt(
-        "Savolni o'zgartiring",
-        editingQuestion.prompt
-      );
-
-      for (let j = 0; j < editingQuestion.option.length; j++) {
-        editingQuestion.option[j] = prompt(
-          "variantni o'zgartiring: ",
-          editingQuestion.option[j]
-        );
-      }
-
-      editingQuestion.answer = prompt(
-        "To'gri variantni o'zgartiring",
-        editingQuestion.answer
-      );
-
-      if (!confirm("Yana boshqa tesni tahrirlaysizmi:")) break;
-    }
+function getQuestionsList(savollar) {
+  let list = "";
+  for (let i = 0; i < savollar.length; i++) {
+    list += i + 1 + "." + savollar[i].question + "\n";
   }
+  return list;
 }
-function deleteQuestion() {
-  for (let i = 0; i < questions.length; i++) {
-    let del = confirm(
-      `O'chirmoqchi  bo'lgan testni tanlang: \n ${questions[i].prompt}`
-    );
-    if (del) {
-      questions.splice(i, 1);
-      if (!confirm("Yana boshqa testni o'chirishni istaysizmi:")) break;
-    }
-  }
+
+function deleteTest(savollar) {
+  let n = prompt(
+    "O'chirmoqchi bo'lgan testni raqamini kiriting\n" +
+      getQuestionsList(savollar)
+  );
+  let newArr = savollar.filter((el, index) => {
+    if (index != n - 1) return el;
+  });
+  questions = newArr;
+}
+function getRandomisedQuestions(questions) {
+  let randomisedQuestions = new Set();
+
+  do {
+    let randomisedIndex = Math.floor(Math.random() * questions.length);
+    randomisedQuestions.add(questions[randomisedIndex]);
+  } while (randomisedQuestions.size != questions.length);
+
+  return randomisedQuestions;
 }
